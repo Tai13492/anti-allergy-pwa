6f170ab6bb4034871a48880e4d931184a5bbbafe
Tai <friendship045@gmail.com>
2019-08-26 14:00:42 +0700

    feat: add temporary scrapping to the app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
-import React from "react";
-import { SearchBar } from "antd-mobile";
+import React, { useState } from "react";
+import { Input } from "antd";
+import { ActivityIndicator, Card, WhiteSpace } from "antd-mobile";
+import Axios from "axios";
+const { Search } = Input;
+// import { SearchBar } from "antd-mobile";
 // Button, WhiteSpace, WingBlank
 // import Axios from "axios";
-import "./App.css";
 
 function App() {
-  // useEffect(() => {
-  //   const testFetch = async () => {
-  //     try {
-  //       const res = await Axios.get(
-  //         "https://www.skincarisma.com/products/shiseido/anessa-perfect-uv-sunscreen-aqua-booster-spf-50-pa/ingredient_list"
-  //       );
-  //       console.log(res, "response");
-  //     } catch (error) {
-  //       const { response } = error;
-  //       console.log(response, "error.response");
-  //     }
-  //   };
-  //   testFetch();
-  // }, []);
+  const [allergies, setAllergies] = useState([]);
+  const [searchBar, setSearchBar] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function computeAllergy(value) {
+    try {
+      setIsLoading(true);
+      const res = await Axios.post(
+        "https://us-central1-anti-allergy-server.cloudfunctions.net/app/compute",
+        { url: value }
+      );
+      if (res.data) {
+        setAllergies(res.data.allergies);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  }
+  console.log(searchBar);
   return (
-    <div>
-      <SearchBar placeholder="Search" />
+    <div
+      style={{
+        maxWidth: 700,
+        margin: "0px auto",
+        backgroundColor: "white",
+        padding: 24
+      }}
+    >
+      <Search
+        value={searchBar}
+        onChange={e => setSearchBar(e.target.value)}
+        placeholder="Enter url"
+        disabled={isLoading}
+        onSearch={value => {
+          setSearchBar("");
+          computeAllergy(value);
+        }}
+        enterButton="Search"
+        size="large"
+      />
+      <WhiteSpace size="md" />
+      {allergies.map(allergy => {
+        return (
+          <React.Fragment key={allergy}>
+            <Card>
+              <Card.Body>
+                <h2> {allergy} </h2>
+              </Card.Body>
+            </Card>
+            <WhiteSpace size="md" />
+          </React.Fragment>
+        );
+      })}
+      <ActivityIndicator toast text="Loading..." animating={isLoading} />
     </div>
   );
 }
